777d90ba9333b2a2cb3516ebf34e5c245e0f3fd1
pook t <geometry1224gd@gmail.com>
2024-05-24 20:14:42 -0700

    fix edge cases for the background color: 0.0.2

--- analyze.js
+++ analyze.js
@@ -1,6 +1,14 @@
 function analyze() {
 let img = document.getElementById('realArt');
 colorjs.average(img, {format:"hex"}).then(color => {  
+    var ogcolor = tinycolor(color);
+    var color = ogcolor.darken(5).toString();
+    if (color == "#ffffff") {
+        color = "#EDEDED";
+    }
+    if (color == "#000000") {
+        color = "#2D2D2D";
+    }
     document.documentElement.style.setProperty("--image-color", color);
 });
 }
